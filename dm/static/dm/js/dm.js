f64611d415aad8565569621736cc40d99d4c49a5
Matthew McCants <mattmccants@gmail.com>
2013-02-12 03:58:14 -0600

    Major JavaScript changes. Makes the UX a lot faster.

--- dm/static/dm/js/dm.js
+++ dm/static/dm/js/dm.js
@@ -1,4 +1,102 @@
 $(function() {
+
+	function Party(resource_uri) {
+		this.resource_uri = resource_uri;
+		this.characters = [];
+	}
+
+	Party.prototype.get_party = function() {
+		console.log('Updating Party Data.');
+		var this_party = this;
+		$.get(this_party.resource_uri, '',
+			function(data) {
+				_.each(data.characters, function(element) {
+					var c = new Character(element);
+					this_party.characters.push(c);
+				});
+				_.each(this_party.characters, function(element){
+					element.get_character();
+				});
+		}, "json");	
+	}
+
+	Party.prototype.update_ui = function() {
+		console.log('Updating all Character UI');
+		_.each(this.characters, function(element) {
+			element.update_ui();
+		});
+	}
+
+	Party.prototype.get_character_by_id = function(character_id) {
+		var character_id = character_id;
+		var character;
+		_.each(this.characters, function(element) {
+			if (element.id == character_id) {
+				character = element;
+			}
+		});
+		return character;
+	}
+
+	function Character(resource_uri) {
+		this.resource_uri = resource_uri;
+	}
+
+	Character.prototype.get_character = function() {
+		var this_character = this;
+		$.get(this_character.resource_uri, '',
+			function(data) {
+				this_character.name = data.name;
+				this_character.hit_points = data.hit_points;
+				this_character.max_hit_points = data.max_hit_points;
+				this_character.id = data.id;
+				console.log('Got character ' + this_character.id);
+				this_character.update_ui();
+		}, "json");
+	}
+
+	Character.prototype.set_character = function() {
+		var this_character = this;
+		$.ajax({
+			url: this_character.resource_uri,
+			type: 'PATCH',
+			contentType: 'application/json',
+			data: JSON.stringify({ "hit_points": this_character.hit_points }),
+			dataType: 'json',
+			processData: false,
+	        beforeSend: function(jqXHR, settings) {
+	        	jqXHR.setRequestHeader('X-CSRFToken', $('input[name=csrfmiddlewaretoken]').val());
+	        },
+	        success: function(data, textStatus, jqXHR) {
+	        	this_character.update_ui();
+	        },
+		});
+
+
+	}
+
+	Character.prototype.update_ui = function() {
+		console.log('Update ui for ' + this.id);
+		var hp_percentage = ((this.hit_points / this.max_hit_points) * 100);
+		$('#hp-text-' + this.id).text(this.hit_points);
+
+		if (hp_percentage <= 30) {
+			$('#hp-bar-parent-' + this.id).removeClass("progress-success progress-warning").addClass("progress-danger");
+		}
+		else if (hp_percentage > 30 && hp_percentage <= 50) {
+			$('#hp-bar-parent-' + this.id).removeClass("progress-success progress-danger").addClass("progress-warning");
+		}
+		else {
+			$('#hp-bar-parent-' + this.id).removeClass("progress-warning progress-danger").addClass("progress-success");
+		}
+		
+		$('#hp-bar-' + this.id).css("width", hp_percentage + "%");
+	}
+
+	my_party = new Party('/dm/api/v1/party/' + $('#party').attr('party-id') + '/');
+
+	$.when(my_party.get_party()).then(my_party.update_ui());
+
 	$('#hp-modal').modal({
 		show: false,
 	});
@@ -7,18 +105,16 @@ $(function() {
 		show: false,
 	});
 
-	update_party($('#party').attr('party-id'));
+	//update_party($('#party').attr('party-id'));
 
 	$('.hp-btn').click(function(){
 		var cid = $(this).attr('character-id');
+		var character = my_party.get_character_by_id(cid);
 		$('#hp-modal-save').removeAttr('party-id');
 		$('#hp-modal-save').attr('character-id', cid);
-		$.get('/DnD/api/v1/character/' + cid + '/','',
-			function(data) {
-				$('#hp-modal-label').text(data.name);
-				$('#hp-input').val(data.hit_points);
-				$('#hp-modal').modal('show');
-		}, "json");
+		$('#hp-modal-label').text(character.name);
+		$('#hp-input').val(character.hit_points);
+		$('#hp-modal').modal('show');
 	});
 
 	$('#hp-btn-party').click(function() {
@@ -36,7 +132,8 @@ $(function() {
 
 	$('#hp-max-btn').click(function() {
 		var cid = $('#hp-modal-save').attr('character-id');
-		get_max_hp('/DnD/api/v1/character/' + cid + '/');
+		var character = my_party.get_character_by_id(cid);
+		$('#hp-input').val(character.max_hit_points);
 	});
 
 
@@ -44,7 +141,9 @@ $(function() {
 		var new_value = $('#hp-input').val();
 		if ($('#hp-modal-save').attr('party-id') == undefined) {
 			var cid = $('#hp-modal-save').attr('character-id');
-			set_hp('/DnD/api/v1/character/' + cid + '/', new_value);
+			var character = my_party.get_character_by_id(cid);
+			character.hit_points = new_value;
+			$.when(character.set_character()).then(character.update_ui());
 		} else {
 			var pid = $('#hp-modal-save').attr('party-id');
 			$.get('/dm/api/v1/party/' + pid + '/', '',
@@ -57,22 +156,6 @@ $(function() {
 		$('#hp-modal').modal('hide');
 	});
 });
-
-function update_party(pid) {
-	$.get('/dm/api/v1/party/' + pid + '/', '',
-		function(data) {
-			_.each(data.characters, function(element) {
-				update_character(element);
-			});
-	}, "json");
-}
-
-function get_max_hp(resource_uri) {
-	$.get(resource_uri,'',
-		function(data) {
-			$('#hp-input').val(data.max_hit_points);
-	}, "json");
-}
 
 function set_hp(resource_uri, hp) {
 	$.ajax({
@@ -89,28 +172,4 @@ function set_hp(resource_uri, hp) {
         	update_character(resource_uri);
         },
 	});
-}
-
-function update_character(resource_uri) {
-	$.get(resource_uri,'',
-		function(data) {
-			console.log(data);
-			var cid = data.id;
-			var hit_points = data.hit_points;
-			var max_hit_points = data.max_hit_points;
-			var hp_percentage = ((hit_points / max_hit_points) * 100);
-			$('#hp-text-' + cid).text(hit_points);
-
-			if (hp_percentage <= 30) {
-				$('#hp-bar-parent-' + cid).removeClass("progress-success progress-warning").addClass("progress-danger");
-			}
-			else if (hp_percentage > 30 && hp_percentage <= 50) {
-				$('#hp-bar-parent-' + cid).removeClass("progress-success progress-danger").addClass("progress-warning");
-			}
-			else {
-				$('#hp-bar-parent-' + cid).removeClass("progress-warning progress-danger").addClass("progress-success");
-			}
-			
-			$('#hp-bar-' + cid).css("width", hp_percentage + "%");
-	}, "json");
 }
