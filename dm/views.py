12b869c1c25e68f7cdbdd5c8c6f6d86a0a4d7c7f
Matthew McCants <mattmccants@gmail.com>
2013-03-16 18:48:42 -0500

    Started refactor of Encounter... again.

--- dm/views.py
+++ dm/views.py
@@ -18,44 +18,13 @@ def encounter(request, encounter_id):
 
     response_dict = {}
     encounter = get_object_or_404(Encounter, id=encounter_id)
-    response_dict['encounter'] = encounter
-    response_dict['party'] = encounter.party
-
-    # TODO: This is not done.
-    # I want to grab all the data before the page is loaded to avoid all this AJAX nonsense.
-
-    participants = EncounterParticipant.objects.filter(encounter=encounter).select_subclasses()
-
-    response_dict['pcs'] = []
-    response_dict['npcs'] = []
-
-    for participant in participants:
-        if hasattr(participant, 'character'):
-            response_dict['pcs'].append(participant)
-        if hasattr(participant, 'npc'):
-            response_dict['npcs'].append(participant)
-
-    EncounterParticipant.objects.filter(encounter=encounter).select_subclasses()
-
-    return render_to_response('dm/encounter.html',
-            response_dict,
-            context_instance=RequestContext(request))
-
-
-@login_required
-def encounter_test(request, encounter_id):
-    if not request.user.is_superuser:
-        raise PermissionDenied
-
-    response_dict = {}
-    encounter = get_object_or_404(Encounter, id=encounter_id)
     
     response_dict['encounter'] = encounter
     response_dict['party'] = encounter.party
     response_dict['pcs'] = encounter.get_pc_participants()
     response_dict['npcs'] = encounter.get_npc_participants()
 
-    return render_to_response('dm/encounter_test.html',
+    return render_to_response('dm/encounter.html',
             response_dict,
             context_instance=RequestContext(request))
 
